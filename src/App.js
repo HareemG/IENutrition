cb41e8963d6ba4fee8282858ded0f693abb0a561
Hareem Gauhar <hareem@Hareems-MacBook-Pro.local>
2020-02-13 10:16:44 -0800

    removed components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import LandingImage from './components/LandingImage';
-import Navbar from './components/Navbar';
-// import About from './components/About';
-// import Contact from './components/Contact';
 
 class App extends Component {
     render () {
         return (
-            <div>
-                <Navbar />
-                <LandingImage />
-            </div>
-        );
-    }
-}
-
-export default App;
+<div></div>
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">IE Nutritionp</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav text-uppercase ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+ 
